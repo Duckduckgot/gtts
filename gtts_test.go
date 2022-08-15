89bde95a35ce257d82aca52f09a81f4725c10615
Duckduckgot <49223254+Duckduckgot@users.noreply.github.com>
2022-08-15 23:03:32 +0900

    Add files via upload

---
+++ gtts_test.go
@@ -0,0 +1,45 @@
+package gtts
+
+import (
+	"github.com/Duckduckgot/gtts/handlers"
+	"github.com/Duckduckgot/gtts/voices"
+
+	"testing"
+)
+
+func TestSpeech_Speak(t *testing.T) {
+	speech := Speech{Folder: "audio", Language: voices.English}
+	speech.Speak("Test")
+}
+
+func TestSpeech_Speak_MPlayer_Handler(t *testing.T) {
+	speech := Speech{Folder: "audio", Language: voices.English, Handler: &handlers.MPlayer{}}
+	speech.Speak("Test")
+}
+
+func TestSpeech_Speak_voice_UkEnglish(t *testing.T) {
+	speech := Speech{Folder: "audio", Language: voices.EnglishUK}
+	speech.Speak("Lancaster")
+}
+
+func TestSpeech_Speak_voice_Japanese(t *testing.T) {
+	speech := Speech{Folder: "audio", Language: voices.Japanese}
+	speech.Speak("Test")
+}
+
+func TestSpeech_CreateSpeechFile(t *testing.T) {
+	speech := Speech{Folder: "audio", Language: voices.English}
+	_, err := speech.CreateSpeechFile("Test", "testfilename")
+	if err != nil {
+		t.Fatalf("CreateSpeechFile fail %v", err)
+	}
+}
+
+func TestSpeech_(t *testing.T) {
+	speech := Speech{Folder: "audio", Language: voices.English}
+	f, err := speech.CreateSpeechFile("Test", "testplay")
+	if err != nil {
+		t.Fatalf("CreateSpeechFile fail %v", err)
+	}
+	speech.PlaySpeechFile(f)
+}
